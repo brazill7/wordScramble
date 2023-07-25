bd1b6e051cb8df40f808a7c5ae757f5ee7e940e2
Maverick <brazillmav@gmail.com>
2023-07-25 11:08:16 -0400

    Initial Commit

---
+++ wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by Maverick Brazill on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
